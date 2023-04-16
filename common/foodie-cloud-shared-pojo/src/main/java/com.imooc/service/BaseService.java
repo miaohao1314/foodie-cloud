0f6c3b80487995ba17f576054c4c2b36a35f4c13
miaohao <>
2023-04-16 15:29:50 +0800

    first commit

---
+++ common/foodie-cloud-shared-pojo/src/main/java/com.imooc/service/BaseService.java
@@ -0,0 +1,25 @@
+package com.imooc.service;
+
+import com.github.pagehelper.PageInfo;
+import com.imooc.pojo.PagedGridResult;
+
+import java.util.List;
+
+
+/**
+ * 包装了PagedGridResult
+ */
+
+public class BaseService {
+
+    public PagedGridResult setterPagedGrid(List<?> list, Integer page) {
+        PageInfo<?> pageList = new PageInfo<>(list);
+        PagedGridResult grid = new PagedGridResult();
+        grid.setPage(page);
+        grid.setRows(list);
+        grid.setTotal(pageList.getPages());
+        grid.setRecords(pageList.getTotal());
+        return grid;
+    }
+
+}
